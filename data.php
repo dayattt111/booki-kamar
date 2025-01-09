fffe26c66289c11171ac29033b1c808d25ff2d64
dayattt111 <awdayaa1@gmail.com>
2025-01-09 14:25:26 +0800

    tambah asset dan gambar kamar

--- data.php
+++ data.php
@@ -4,7 +4,7 @@
         "nama" => "Produk A",
         "deskripsi" => "Deskripsi produk A.",
         "harga" => 2000,
-        "gambar" => "gambar2.jpg"
+        "gambar" => "./asset/k0.jpg"
     ],
 
     [
@@ -13,10 +13,12 @@
         "Produk B",
         "deskripsi" => "Deskripsi produk B.",
         "harga" => 12000,
-        "gambar" => "gambar3.jpg"
+        "gambar" => "./asset/k1.jpg"
     ],
-    ["id" => 3, "nama" => "Produk C", "deskripsi" => "Deskripsi produk C.", "harga" => 22000, "gambar" => "gambar4.jpg"],
-    ["id" => 4, "nama" => "Produk D", "deskripsi" => "Deskripsi produk D.", "harga" => 32000, "gambar" => "gambar5.jpg"],
-    ["id" => 5, "nama" => "Produk D", "deskripsi" => "Deskripsi produk D.", "harga" => 42000, "gambar" => "gambar1.jpg"],
-    ["id" => 6, "nama" => "Produk D", "deskripsi" => "Deskripsi produk D.", "harga" => 42000, "gambar" => "gambar1.jpg"],
+    ["id" => 3, "nama" => "Produk C", "deskripsi" => "Deskripsi produk C.", "harga" => 22000, "gambar" => "./asset/k2.jpg"],
+    ["id" => 4, "nama" => "Produk D", "deskripsi" => "Deskripsi produk D.", "harga" => 32000, "gambar" => "./asset/k3.jpg"],
+    ["id" => 5, "nama" => "Produk D", "deskripsi" => "Deskripsi produk D.", "harga" => 42000, "gambar" => "./asset/k4.jpg"],
+    ["id" => 6, "nama" => "Produk D", "deskripsi" => "Deskripsi produk D.", "harga" => 42000, "gambar" => "./asset/k5.jpg"],
+    ["id" => 6, "nama" => "Produk D", "deskripsi" => "Deskripsi produk D.", "harga" => 42000, "gambar" => "./asset/k6.jpg"],
+    ["id" => 6, "nama" => "Produk D", "deskripsi" => "Deskripsi produk D.", "harga" => 42000, "gambar" => "./asset/k7.jpg"],
 ];
